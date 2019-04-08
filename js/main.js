c67c98b4633e0f927e14a4dd8c724d5cd8d67803
ZylionNorth <49384654+ZylionNorth@users.noreply.github.com>
2019-04-08 14:12:02 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+var newmeme = `hope i win i never won before only got 2nd
+im a super fast big larru
+qui
+kiddo
+HE Y'wd
+gg we win get rekt m8 gg10 ur bad kiddo
+gg we quub
+" _) PO
+reported for lemon
+kcp,e
+its BEDD time
+oes this look look
+rpoeokled;d
+shut the hell up no one cares
+spung bab skwaer pent
+NIEU
+oh snap
+okatbugb'
+bittog
+POEKASE
+foke
+stu[poipdumbba doicot
+stupidumbadiot
+AAAAAAAAAAAND aug
+Querétaro Otomi
+queer boi
+goddamkn idiot moron bo i queeaefr foucking mroeon sisidoot suspitdusbitch ass fuckiner
+AINZCHE
+why is someone morthed as a blze flying around in the viod
+gooey
+put the jew
+ah noe
+LA LA LA LA LA
+akuchuli
+ecpaplod
+three two one new zealand
+HEKKI OUCJKE
+boy
+band
+whee
+du
+anguy
+hlehorf
+get dumb hacker
+lord farquaad sends his goddamn regards
+stupuid
+give us we are shit
+mongoloid
+but i've killed many flies!
+did i stute?
+shut the hell your mouth
+moronloid
+mbah a moute
+that is true! you're not wrong!
+o waro
+DUMPHEAD
+nogga noga
+higlet
+lol he was legitimately playing civ this entire time
+head
+autismo
+muppet
+mecum
+wummus
+know wah meen?
+i'm ata
+`
+
+function main() {
+    let memes = newmeme.split("\n");
+    let memetext = memes.join("<br>");
+    let cardData = document.getElementById("card-data");
+    cardData.innerHTML = memetext;
+}
+
+window.addEventListener("load", main);
