005ac0a8a35539c2af3cca69955b7c26c45f2205
ZylionNorth <49384654+ZylionNorth@users.noreply.github.com>
2020-03-26 17:19:51 -0400

    Add egirl test

---
+++ egirl/main.js
@@ -0,0 +1,88 @@
+const egirlThreshold = 20;
+const questionHTML = `
+<label class="checkbox-container">
+    {}
+    <input type="checkbox" class="question" onclick="updateScore();">
+    <span class="checkmark"></span>
+</label>`;
+
+function updateScore(updateLink) {
+    var questionsDiv = document.getElementById("questions");
+    var totalQuestions = questionsDiv.children.length;
+    var result = 0;
+    for (var question of questionsDiv.getElementsByClassName("question")) {
+        if (question.checked) {
+            result++;
+        }
+    }
+
+    var percentage = Math.round((result / egirlThreshold) * 100);
+    document.getElementById("percent").innerText = percentage + "%";
+    document.getElementById("score").innerText = result;
+
+    if (percentage >= 100) {
+        document.getElementById("egirl-img").classList.remove("hidden");
+    } else {
+        document.getElementById("egirl-img").classList.add("hidden");
+    }
+
+    encodeAnswers();
+}
+
+function encodeAnswers() {
+    var questionsDiv = document.getElementById("questions");
+    var answers = "";
+    for (var question of questionsDiv.getElementsByClassName("question")) {
+        if (question.checked) {
+            answers += "1";
+        } else {
+            answers += "0";
+        }
+    }
+    var result = parseInt(answers, 2).toString(16);
+
+    var url = new URL(window.location.href);
+    url.searchParams.set("results", result);
+    document.getElementById("share-link").value = url.href;
+}
+
+function decodeAnswers() {
+    var url = new URL(window.location.href);
+    result = url.searchParams.get("results");
+    if (result != null) {
+        var questionsDiv = document.getElementById("questions");
+        var totalQuestions = questionsDiv.children.length;
+        var answers = (parseInt(result, 16).toString(2)).padStart(totalQuestions, "0");
+        var questionList = questionsDiv.getElementsByClassName("question")
+        for (var i = 0; i < totalQuestions; i++) {
+            if (answers[i] == "0") {
+                questionList[i].checked = false;
+            } else {
+                questionList[i].checked = true;
+            }
+        }
+    }
+}
+
+function copyLink() {
+    var linkBox = document.getElementById("share-link");
+    linkBox.removeAttribute("disabled");
+    linkBox.select();
+    linkBox.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    linkBox.setAttribute("disabled", "disabled");
+}
+
+function main() {
+    var questionsDiv = document.getElementById("questions");
+    var questionsSplit = questions.split("\n");
+    for (var question of questionsSplit) {
+        questionsDiv.innerHTML += questionHTML.replace("{}", question);
+    }
+    document.getElementById("total").innerText = questionsSplit.length;
+
+    decodeAnswers();
+    encodeAnswers();
+}
+
+window.addEventListener("load", main);
---
+++ egirl/questions.js
@@ -0,0 +1,52 @@
+const questions = `listens to billie eilish
+currently in any form of e-relationship
+owns a vape
+discord open on pc
+tons of snapchat streaks
+has recently talked to an eboy
+posts pictures of herself then calls herself ugly/fat when she knows she's not actually ugly/fat but wants attention in the comments
+has sent nudes or seductive explicit pictures
+always has a filter on
+thrives off of attention
+always complains
+always depressed
+has had e-sex
+lowkey competes with other egirls
+roasts edaters but edates
+claims not to be an egirl
+has said "LMFAOFOAOAO," "IM-," "IM WHEEZING," or any variation of those phrases
+has said "SKSKSKSKS" before, or some variation of it
+all lowercase name
+tumblr, anime, or one of those barbie dolls with hearts profile pictures somewhere
+flirts a lot online
+1-2 month e-relationships
+pro-choice
+claims not to play minecraft
+has turned profile picture to black for a period of time
+has another instagram "beauty" or makeup page
+wears too much winged eyeliner all the time
+gets carried by eboys in games
+types without punctuation
+listens to X or peep
+says lmao a lot
+posts explicit photos on her spam
+more online friends than irl
+never dated irl
+is lowkey a snake
+listens to ariana grande
+hides behind minecraft hacker egoys
+matching minecraft skins
+eboys name in insta bio
+believes in astrology signs
+leaves discord when depressed
+fingers herself 5+ times a week
+probably mentally unstable
+watches james charles
+has eboys buy them stuff
+streaks in an "s"
+has never flirted irl
+talks shit about other egirls
+always muted in calls
+apple earbuds as a mic
+is socially awkward irl
+is a feminist`;
